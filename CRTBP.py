44e914c670882c08355fd2559ff5bb9049f227bd
tobiastanis <t.p.tanis@student.tudelft.nl>
2022-03-16 15:46:08 +0100

    crtb

--- CRTBP.py
+++ CRTBP.py
@@ -3,6 +3,10 @@ Some text about the CRTBP
 """
 import numpy as np
 from tudatpy.kernel.interface import spice_interface
+from scipy.integrate import odeint
+from tudatpy.kernel import constants
+spice_interface.load_standard_kernels()
+
 
 def crtbp(x, t, mu):
     r1 = np.sqrt((mu + x[0]) ** 2 + x[1] ** 2 + x[2] ** 2)              #Distance to Primary (Earth)
@@ -20,3 +24,23 @@ def crtbp(x, t, mu):
             ]
     return xdot
 
+####### Normalizing Units #######
+G = constants.GRAVITATIONAL_CONSTANT
+M_Earth = spice_interface.get_body_gravitational_parameter("Earth")/G
+M_Moon = spice_interface.get_body_gravitational_parameter("Moon")/G
+
+L_char = 384400e3
+m_char = M_Earth+M_Moon
+t_char = np.sqrt(L_char**3/(constants.GRAVITATIONAL_CONSTANT*m_char))
+v_char = L_char/t_char
+mu = spice_interface.get_body_gravitational_parameter("Moon")/(spice_interface.get_body_gravitational_parameter("Earth")+spice_interface.get_body_gravitational_parameter("Moon"))
+
+
+t_span = np.linspace(0, 20/t_char, 1000)
+x_ini = np.array([1.1435, 0, -0.1579, 0, -0.2220, 0])
+
+states = odeint(crtbp, x_ini, t_span, args=(mu,), rtol=1e-12, atol=1e-12)
+import matplotlib.pyplot as plt
+print(constants.GRAVITATIONAL_CONSTANT)
+
+plt.show()
