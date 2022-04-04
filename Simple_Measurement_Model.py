86c4a6942fd923f02354786996f9609c5cdccf5e
tobiastanis <t.p.tanis@student.tudelft.nl>
2022-04-04 14:46:05 +0200

    measurementmodel

--- Simple_Measurement_Model.py
+++ Simple_Measurement_Model.py
@@ -7,26 +7,30 @@ import matplotlib.pyplot as plt
 
 print("Running [Simple_Measurement_Model.py]")
 time = LUMIO_LLO_propagation.time
-
+output = LUMIO_LLO_propagation.output
+position_LUMIO_wrt_Moon = output[:, 28:31]
 x_Moon = LUMIO_LLO_propagation.X_Moon
-states_LUMIO = LUMIO_LLO_propagation.states[:, 0:6]
 states_LLO_wrt_Moon = LUMIO_LLO_propagation.states[:, 6:12]
-states_LLO = np.add(states_LLO_wrt_Moon, x_Moon)
+position_LLO_wrt_Moon = states_LLO_wrt_Moon[:, 3:6]
 
-LOS_vector = np.subtract(states_LUMIO[:, 0:3], states_LLO[:, 3:6])
+LOS_vector = np.subtract(position_LUMIO_wrt_Moon, position_LLO_wrt_Moon)
 intersatelltie_distance = np.linalg.norm(LOS_vector, axis=1)
 
-measurement_noise = np.random.normal(0, 10e5, len(time))
+measurement_noise = np.random.normal(0, 10e4, len(time))
 
 measured_distance = np.add(intersatelltie_distance, measurement_noise)
 
 plt.figure()
 plt.plot(time, intersatelltie_distance, color='red')
 plt.plot(time, measured_distance, color='blue')
-plt.title('((Measured) Intersatellite distance')
+plt.title('(Measured) Intersatellite distance')
 plt.xlabel('Time [days]')
 plt.ylabel('Distance [m]')
 plt.legend(['Nominal intersatellite distance', 'Measured intersatellite distance'])
+
+print('Maximum intersatellite distance \n', max(intersatelltie_distance)*10**-3, '[km]')
+print('Minimum intersatellite distance \n', min(intersatelltie_distance)*10**-3, '[km]')
+
 
 print('[Simple_Measurement_Model.py] successfully ran')
 plt.show()
